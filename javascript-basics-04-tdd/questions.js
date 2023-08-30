8924b667e84f7472b7b80ff5b43ad72ba4ebf2c6
rafikxhawshar <rafikhawshar@gmail.com>
2023-08-30 13:58:23 +0300

    Implement various utility functions for string manipulation, math calculations, and array operations

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,101 +1,121 @@
-
 const stringSize = (text) => {
-
-}
-const replaceCharacterE = (text) => {
-
-}
-const concatString = (text1, text2) => {
-
-}
-const showChar5 = (text) => {
-
-}
-const showChar9 = (text) => {
-
-}
-const toCapitals = (text) => {
-
-}
-const toLowerCase = (text) => {
-
-}
-const removeSpaces = (text) => {
-
-}
-const IsString = (text) => {
-
-}
-
-const getExtension = (text) => {
-
-}
-const countSpaces = (text) => {
-
-}
-const InverseString = (text) => {
-
-}
-
-const power = (x, y) => {
-
-}
-const absoluteValue = (num) => {
-
-}
-const absoluteValueArray = (array) => {
-
-}
-const circleSurface = (radius) => {
-
-}
-const hypothenuse = (ab, ac) => {
-
-}
-const BMI = (weight, height) => {
-
-}
-
-const createLanguagesArray = () => {
-
-}
-
-const createNumbersArray = () => {
-
-}
-
-const replaceElement = (languages) => {
-
-}
-
-const addElement = (languages) => {
-
-}
-
-const addNumberElement = (numbers) => {
-
-}
-
-const removeFirst = (languages) => {
-
-}
-
-const removeLast = (languages) => {
-
-}
-
-const convertStrToArr = (social_arr) => {
-
-}
-
-const convertArrToStr = (languages) => {
-
-}
-
-const sortArr = (social_arr) => {
-
-}
-
-const invertArr = (social_arr) => {
-
-}
+    return text.length;
+  }
+  
+  const replaceCharacterE = (text) => {
+    return text.replace('e', ' ');
+  }
+  
+  const concatString = (text1, text2) => {
+    return text1 + text2;
+  }
+  
+  const showChar5 = (text) => {
+    return text.charAt(4);
+  }
+  
+  const showChar9 = (text) => {
+    return text.slice(0, 9);
+  }
+  
+  const toCapitals = (text) => {
+    return text.toUpperCase();
+  }
+  
+  const toLowerCase = (text) => {
+    return text.toLowerCase();
+  }
+  
+  const removeSpaces = (text) => {
+    return text.trim();
+  }
+  
+  const IsString = (text) => {
+    return typeof text === 'string';
+  }
+  
+  const getExtension = (text) => {
+    return text.split('.').pop();
+  }
+  
+  const countSpaces = (text) => {
+    return (text.match(/ /g) || []).length;
+  }
+  
+  const InverseString = (text) => {
+    return text.split('').reverse().join('');
+  }
+  
+  const power = (x, y) => {
+    return Math.pow(x, y);
+  }
+  
+  const absoluteValue = (num) => {
+    return Math.abs(num);
+  }
+  
+  const absoluteValueArray = (array) => {
+    return array.map(Math.abs);
+  }
+  
+  const circleSurface = (radius) => {
+    return Math.round(Math.PI * Math.pow(radius, 2));
+  }
+  const hypothenuse = (ab, ac) => {
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
+  }
+  
+  const BMI = (weight, height) => {
+    const bmi = weight / (height * height);
+    return parseFloat(bmi.toFixed(2));
+  }
+  
+  const createLanguagesArray = () => {
+    return ['Html', 'CSS', 'Java', 'PHP'];
+  }
+  
+  const createNumbersArray = () => {
+    return [0, 1, 2, 3, 4, 5];
+  }
+  
+  const replaceElement = (languages) => {
+    languages[2] = 'Javascript';
+    return languages;
+  }
+  
+  const addElement = (languages) => {
+    languages.push('Ruby', 'Python');
+    return languages;
+  }
+  
+  const addNumberElement = (numbers) => {
+    numbers.unshift(-2, -1);
+    return numbers;
+  }
+  
+  const removeFirst = (languages) => {
+    languages.shift();
+    return languages;
+  }
+  
+  const removeLast = (languages) => {
+    languages.pop();
+    return languages;
+  }
+  
+  const convertStrToArr = (social_arr) => {
+    return social_arr.split(',');
+  }
+  
+  const convertArrToStr = (languages) => {
+    return languages.join(',');
+  }
+  
+  const sortArr = (social_arr) => {
+    return social_arr.sort();
+  }
+  
+  const invertArr = (social_arr) => {
+    return social_arr.reverse();
+  }
+  
