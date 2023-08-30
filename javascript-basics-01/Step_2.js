ef37798fcc2725161203a043e519ada6415cafa6
rafikxhawshar <rafikhawshar@gmail.com>
2023-08-30 10:08:26 +0300

    changed to make it same as the photo

--- javascript-basics-01/Step_2.js
+++ javascript-basics-01/Step_2.js
@@ -1,4 +1,4 @@
 var name = "Doe";
 var surname = "John";
 var city = "New-York";
-alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
+alert("Nom: " + name + "\nPrenom: " + surname + "\nVillie: " + city);
